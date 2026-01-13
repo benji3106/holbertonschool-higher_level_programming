0a50a20e60ba359280723fc8049dec3e443ebdfb
benji3106 <benjamin3106@live.fr>
2026-01-13 18:53:39 +0100

    fix: refactor print logic for clarity in 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for loop in range(100):
-    print("{}".format(loop), end=', ') if loop < 99 else print("{}".format(loop), end='')
+    if loop < 99:
+        print("{}".format(loop), end=', ')
+    else:
+        print("{}".format(loop), end='')
