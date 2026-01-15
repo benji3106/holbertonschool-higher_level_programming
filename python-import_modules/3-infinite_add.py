2a8d5836d6560510452fa2bb8c629343813402cd
benji3106 <benjamin3106@live.fr>
2026-01-15 17:30:03 +0100

    fix: convert indentation in spaces

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 from sys import argv
 
 if __name__ == "__main__":
-	total = 0
-	for i in range(1, len(argv)):
-		total += int(argv[i])
-	print(total)
+    total = 0
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+    print(total)
