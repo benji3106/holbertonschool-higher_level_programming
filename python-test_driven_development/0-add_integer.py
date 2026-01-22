972cc6796d7232cd77ea14c39fc27ecb3612604f
benji3106 <benjamin3106@live.fr>
2026-01-22 16:19:39 +0100

    fix: correct indentation in add_integer function's comments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,8 +8,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-	# Reject NaN / Infinity (float overflow & invalid float values)
+
+    # Reject NaN / Infinity (float overflow & invalid float values)
     if isinstance(a, float) and (a != a or a in (float("inf"), float("-inf"))):
         raise TypeError("a must be an integer")
     if isinstance(b, float) and (b != b or b in (float("inf"), float("-inf"))):
