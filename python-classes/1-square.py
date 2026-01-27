bd513ea421d027fc5e561fd64f0ed7b5043ca031
benji3106 <benjamin3106@live.fr>
2026-01-27 15:55:22 +0100

    feat: convert indentation to space

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
-	"""A class that defines a square with a private size attribute."""
-	def __init__(self, size):
-		"""Initialize the square with a given size.
+    """A class that defines a square with a private size attribute."""
+    def __init__(self, size):
+        """Initialize the square with a given size.
 
-		Args:
-			size (int): The size of the square.
-		"""
-		self.__size = size
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
