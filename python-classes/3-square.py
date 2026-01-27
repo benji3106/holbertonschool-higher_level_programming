b0daf597a3ae7e6f27cd86ebb996ed89eee32c3b
benji3106 <benjamin3106@live.fr>
2026-01-27 15:57:22 +0100

    feat: create a class Square that defines a square with Public instance method that return the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Square:
+	"""A class that defines a square with a private size attribute."""
+	def __init__(self, size=0):
+		"""Initialize the square with a given size.
+
+		Args:
+			size (int): The size of the square.
+		"""
+
+		if not isinstance(size, int):
+			raise TypeError("size must be an integer")
+		if size < 0:
+			raise ValueError("size must be >= 0")
+		self.__size = size
+	def area(self):
+		"""Calculate and return the area of the square.
+
+		Returns:
+			int: The area of the square.
+		"""
+		return self.__size ** 2
