cf874f2823d1af2727c738311771efcb8a9743d3
benji3106 <benjamin3106@live.fr>
2026-01-28 14:15:32 +0100

    fix: modification for pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
+
+
 class Square:
     """A class that defines a square with a private size attribute."""
+
     def __init__(self, size):
         """Initialize the square with a given size.
 
