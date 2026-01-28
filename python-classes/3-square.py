414c7061dcd064dc7217d94c5a413f07870bed5e
benji3106 <benjamin3106@live.fr>
2026-01-28 09:19:50 +0100

    fix: convert indentation to spaces

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 class Square:
-	"""A class that defines a square with a private size attribute."""
-	def __init__(self, size=0):
-		"""Initialize the square with a given size.
+    """A class that defines a square with a private size attribute."""
+    def __init__(self, size=0):
+        """Initialize the square with a given size.
 
-		Args:
-			size (int): The size of the square.
-		"""
+        Args:
+            size (int): The size of the square.
+        """
 
-		if not isinstance(size, int):
-			raise TypeError("size must be an integer")
-		if size < 0:
-			raise ValueError("size must be >= 0")
-		self.__size = size
-	def area(self):
-		"""Calculate and return the area of the square.
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-		Returns:
-			int: The area of the square.
-		"""
-		return self.__size ** 2
+    def area(self):
+        """Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
