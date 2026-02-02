2fc72e7edf21b5e6dcb7db48b631909ca384fcad
benji3106 <benjamin3106@live.fr>
2026-02-02 15:11:52 +0100

    feat: Add Rectangle with width and height validation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,14 +2,13 @@
 class BaseGeometry:
     """BaseGeometry class."""
 
-
     def area(self):
-        """Calculate area of geometry."""
+        """Raises an exception because area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-            """Validates that value is an integer greater than 0."""
-            if type(value) is not int:
-                raise TypeError(f"{name} must be an integer")
-            if value <= 0:
-                raise ValueError(f"{name} must be greater than 0")
+        """Validates that value is an integer greater than 0."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
