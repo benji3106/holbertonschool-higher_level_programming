43556916f1990732ddfd563488e581e437d0e2db
benji3106 <benjamin3106@live.fr>
2026-02-02 14:46:34 +0100

    feat: Add BaseGeometry integer validation and tests

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """BaseGeometry class."""
+
+
+    def area(self):
+        """Calculate area of geometry."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+            """Validates that value is an integer greater than 0."""
+            if type(value) is not int:
+                raise TypeError(f"{name} must be an integer")
+            if value <= 0:
+                raise ValueError(f"{name} must be greater than 0")
