84ad804481d0ae936d1f08c600ab0248fe4863c0
benji3106 <benjamin3106@live.fr>
2026-02-02 10:09:49 +0100

    feat: create a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class MyList(list):
+    """MyList class that inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list sorted in ascending order"""
+        print(sorted(self))
