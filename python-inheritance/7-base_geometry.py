d9a5206e683ded94661b591d5f8542bec283481a
benji3106 <benjamin3106@live.fr>
2026-02-04 09:55:35 +0100

    fix: add better docstring

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Module that defines Rectangle class."""
+"""Module that defines BaseGeometry class."""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """BaseGeometry class."""
 
+    def area(self):
+        """Raise an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
 
-class Rectangle(BaseGeometry):
-    """Rectangle class."""
-
-    def __init__(self, width, height):
-        """Initialize a rectangle with validated width and height."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    def integer_validator(self, name, value):
+        """Validate that value is an integer greater than 0."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines Rectangle class."""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry."""
+    """Rectangle class."""
 
     def __init__(self, width, height):
-        """Initialize rectangle with width and height."""
+        """Initialize a rectangle with validated width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
