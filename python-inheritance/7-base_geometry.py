ff990abb696a7222f72a9e12c336b85e6d6a676c
benji3106 <benjamin3106@live.fr>
2026-02-04 09:50:56 +0100

    fix: add better docstring

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""Module that defines BaseGeometry class."""
+
+
 class BaseGeometry:
     """BaseGeometry class."""
 
     def area(self):
-        """Raises an exception because area is not implemented."""
+        """Raise an exception because area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is an integer greater than 0."""
+        """Validate that value is an integer greater than 0."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
