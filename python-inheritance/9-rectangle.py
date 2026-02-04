447bb2119cf1ec0d3816830d73c3239c7778a40c
benji3106 <benjamin3106@live.fr>
2026-02-04 09:56:55 +0100

    fix: add better docstring

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+"""Module that defines Rectangle class."""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry."""
+    """Rectangle class."""
 
     def __init__(self, width, height):
-        """Initialize rectangle with width and height."""
+        """Initialize a rectangle with validated width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """Calculate area of the rectangle."""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """Return string representation of the rectangle."""
+        """Return the string representation of the rectangle."""
         return f"[Rectangle] {self.__width}/{self.__height}"
