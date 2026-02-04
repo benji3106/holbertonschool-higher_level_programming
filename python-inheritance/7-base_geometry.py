381ac5446103865cee055c14ed46e37425d6678b
benji3106 <benjamin3106@live.fr>
2026-02-04 09:54:12 +0100

    fix: add better docstring

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Module that defines BaseGeometry class."""
+"""Module that defines Rectangle class."""
 
 
-class BaseGeometry:
-    """BaseGeometry class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        """Raise an exception because area is not implemented."""
-        raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """Validate that value is an integer greater than 0."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+class Rectangle(BaseGeometry):
+    """Rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle with validated width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
