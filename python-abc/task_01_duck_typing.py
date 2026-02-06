48b40e3b0d42e156c05700184298d80d5c74cbd4
benji3106 <benjamin3106@live.fr>
2026-02-06 11:59:44 +0100

    fix: modify condition for radius negative

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -37,20 +37,12 @@ class Circle(Shape):
         """
         Initialize the circle with a radius.
         """
-        if radius < 0:
-            raise ValueError("radius must be >= 0")
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
-        """
-        Returns the area of the circle.
-        """
         return math.pi * self.radius ** 2
 
     def perimeter(self):
-        """
-        Returns the perimeter of the circle.
-        """
         return 2 * math.pi * self.radius
 
 
