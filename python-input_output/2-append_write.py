a387ff453745a68c10c6e7c08dd6f12d4de51ef7
benji3106 <benjamin3106@live.fr>
2026-02-11 09:10:42 +0100

    fix: Add append_write function

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that defines a function that appends a string to a
+UTF-8 text file and returns the number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Append a string to a UTF-8 text file and return the number
+    of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
