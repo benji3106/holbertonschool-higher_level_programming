b4db85356793d6df4384f112b26f6bd25726f226
benji3106 <benjamin3106@live.fr>
2026-02-11 09:05:35 +0100

    fix: pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Module that defines a function that reads a text file (UTF8) and prints it to stdout."""
+"""
+Module that defines a function that reads a text file (UTF8)
+and prints it to stdout.
+"""
 
 
 def read_file(filename=""):
-    """Read a text file (UTF8) and print it to stdout."""
+    """Read a text file and print it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
