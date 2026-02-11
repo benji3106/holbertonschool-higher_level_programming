941292ce0d3fc25f20e04da7b9c4673cf7b326d4
benji3106 <benjamin3106@live.fr>
2026-02-11 11:27:30 +0100

    fix: Fix module imports to use __import__ for JSON functions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Script that adds all arguments to a Python list and saves
-them to a JSON file.
+Script that adds all arguments to a Python list, and then saves them
+to a JSON file (add_item.json).
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
