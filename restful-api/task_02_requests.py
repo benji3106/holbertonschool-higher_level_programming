89283d8b946e672db38818002f8be06d473f31b8
benji3106 <benjamin3106@live.fr>
2026-02-18 10:36:33 +0100

    fix: pycodestyle modification

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,13 +3,9 @@ import csv
 
 
 def fetch_and_print_posts():
-    """
-    Fetch all posts from JSONPlaceholder and print their titles.
-    """
-
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
-    print(f"Status Code: {response.status_code}")
+    print("Status Code:", response.status_code)
 
     if response.status_code == 200:
         posts = response.json()
@@ -19,29 +15,17 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
-    """
-    Fetch all posts and save selected data into a CSV file.
-    """
-
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     if response.status_code == 200:
         posts = response.json()
 
-        structured_data = [
-            {
-                "id": post["id"],
-                "title": post["title"],
-                "body": post["body"]
-            }
-            for post in posts
-        ]
+        file = open("posts.csv", "w", newline="", encoding="utf-8")
+        writer = csv.writer(file)
 
-        with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
-            fieldnames = ["id", "title", "body"]
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(["id", "title", "body"])
 
-            writer.writeheader()
-            writer.writerows(structured_data)
+        for post in posts:
+            writer.writerow([post["id"], post["title"], post["body"]])
 
-        print("Data successfully saved to posts.csv")
+        file.close()
