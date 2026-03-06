55f4af5836e530d3081996a3774e6cc1659755f1
benji3106 <benjamin3106@live.fr>
2026-03-06 10:57:52 +0100

    feat: create  python file that contains the class definition of a State

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines the State class and Base."""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """State class that maps to the states table."""
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
