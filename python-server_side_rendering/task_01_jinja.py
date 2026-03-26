e31e290950a3896c80bca2b9117cdde9951e5076
benji3106 <benjamin3106@live.fr>
2026-03-26 11:26:39 +0100

    fix: modification for docstring

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-"""Basic Flask application with Jinja templates"""
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,19 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    """Render home page"""
     return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-    """Render about page"""
     return render_template('about.html')
 
 
 @app.route('/contact')
 def contact():
-    """Render contact page"""
     return render_template('contact.html')
 
 
